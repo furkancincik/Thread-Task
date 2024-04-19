2b0ab5ba97fea6e9ac2d5efdf24c95c83f3e887e
Furkan Çinçik <furkancincik1999@hotmail.com>
2024-04-19 23:15:58 +0300

    Patika/task

---
+++ src/Calculate.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculate implements Runnable {
+    private static List<Integer> oddNumbers;
+    private static List<Integer> evenNumbers;
+    private List<Integer> numbers;
+    String name;
+    int i = 0;
+
+
+    public Calculate(List<Integer> numbers, String name) {
+        this.numbers = numbers;
+        this.name = name;
+        oddNumbers = new ArrayList<>();
+        evenNumbers = new ArrayList<>();
+    }
+
+
+    @Override
+    public void run() {
+        System.out.println(this.name + " Calisiyor.");
+        for (int number : numbers) {
+            addNumber(number);
+        }
+        System.out.println(this.name+" Toplam tek: "+oddNumbers.size()+" Toplam çift: "+evenNumbers.size());
+    }
+
+    public void addNumber(int number) {
+        if (number % 2 == 0){
+            synchronized (evenNumbers){
+                evenNumbers.add(number);
+            }
+        }else {
+            synchronized (oddNumbers){
+                oddNumbers.add(number);
+            }
+        }
+    }
+
+    public static List<Integer> getOddNumbers() {
+        return oddNumbers;
+    }
+
+    public static List<Integer> getEvenNumbers() {
+        return evenNumbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
